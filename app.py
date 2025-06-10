f1f66c83271dcf1160dcf4dccc473fe71e268c48
jjgong12 <65600544+jjgong12@users.noreply.github.com>
2025-06-10 15:19:56 +0900

    app.py

--- app.py
+++ app.py
@@ -239,5 +239,5 @@ if __name__ == '__main__':
     print("   GET  / - 서비스 정보")
     
     import os
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
